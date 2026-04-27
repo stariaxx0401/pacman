7f55bb308528adbd4e9d5cc29137238a0d79f82b
Serhat Türmüş <sturmus8@gmail.com>
2026-04-27 18:18:43 +0300

    harita grid sistemi tasarlandi

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,41 @@
 #include <SFML/Graphics.hpp> 
+
+// hücre boyutu 
+const int CELL_SIZE = 20; 
+
+// harita boyutu
+const int MAP_ROWS = 21;
+const int MAP_COLS = 19;
+
+// haritanın grid yapısını matematiksel olarak tanımlar.
+// yol=0 duvar=1
+int map[MAP_ROWS][MAP_COLS] = {
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1},
+    {1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,0,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,0,1},
+    {1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1},
+    {1,1,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,1,1},
+    {1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,1},
+    {1,1,1,1,0,1,0,1,1,0,1,1,0,1,0,1,1,1,1},
+    {0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0},
+    {1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1},
+    {1,1,1,1,0,1,0,0,0,0,0,0,0,1,0,1,1,1,1},
+    {1,1,1,1,0,1,0,1,1,1,1,1,0,1,0,1,1,1,1},
+    {1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1},
+    {1,0,1,1,0,1,1,1,0,1,0,1,1,1,0,1,1,0,1},
+    {1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,1},
+    {1,1,0,1,0,1,0,1,1,1,1,1,0,1,0,1,0,1,1},
+    {1,0,0,0,0,1,0,0,0,1,0,0,0,1,0,0,0,0,1},
+    {1,0,1,1,1,1,1,1,0,1,0,1,1,1,1,1,1,0,1},
+    {1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+};
+
 int main() {
-    // 800x600 piksel boyutunda pencere oluşturmak için
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Pacman Test");
+    // 800x600 yerine grid yapısına tam oturan pencere oluşturuldu
+    sf::RenderWindow window(sf::VideoMode(MAP_COLS * CELL_SIZE, MAP_ROWS * CELL_SIZE), "Pacman Test");
 
     // pencere açık olduğu sürece çalışır
     while (window.isOpen()) {
@@ -14,10 +48,10 @@ int main() {
                 window.close();
         }
 
-        // Bir önceki framei siler yoksa eski görüntü üstüne çizilir
+        // bir önceki framei siler yoksa eski görüntü üstüne çizilir
         window.clear(sf::Color::Black);
 
-        // Pacman, duvarlar, hayaletler vs. olacak
+        // grid sistemi tasarlandı 
 
         window.display();
     }
