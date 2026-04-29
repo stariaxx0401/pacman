0ff29a7220f9e8726a9263e117bfb830872b7407
Serhat Türmüş <sturmus8@gmail.com>
2026-04-29 18:34:58 +0300

    harita duvarlari cizildi

--- src/main.cpp
+++ src/main.cpp
@@ -52,6 +52,25 @@ int main() {
         window.clear(sf::Color::Black);
         // Duvarlar için kare şeklinde bir görsel şablon oluşturuldu(-1.0f duvarlar arasında ince siyah çizgi yapısı sağlar cellsize olsaydı birbirine girerdi )
         sf::RectangleShape wall(sf::Vector2f(CELL_SIZE - 1.0f, CELL_SIZE - 1.0f));
+         // Duvarları mavi renge boyar
+        wall.setFillColor(sf::Color::Blue);
+
+        // Her satır ve sütunu dolaşarak haritayı ekrana çizer
+        for (int row = 0; row < MAP_ROWS; row++)
+        {
+            for (int col = 0; col < MAP_COLS; col++)
+            {
+                // map te 1 varsa o duvar
+                if (map[row][col] == 1)
+                {
+                    // col*CELL_SIZE = yatay konum
+                    // row*CELL_SIZE = dikey konum
+                    wall.setPosition(col * CELL_SIZE, row * CELL_SIZE);
+                    window.draw(wall);
+                }
+            }
+        }
+
         
 
         window.display();
